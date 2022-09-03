4f56d8ffd9ad4ca7ed2f1f05261e9571b4ad129f
frako23 <frako23@gmail.com>
2022-09-03 14:47:15 +0000

    se carga el proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["el", "nuestro", "mi", "tu", "su"];
+  let adj = ["gran", "generoso", "fastuoso", "elegante", "inutil"];
+  let noun = ["susto", "secreto", "mentira", "pelon", "metidadepata"];
+  let extensiones = [".com", ".net", ".us", ".io", ".dev"];
+
+  let excuseP = document.querySelector("#excuse");
+  excuseP.textContent =
+    pronoun[Math.floor(Math.random() * 5)] +
+    adj[Math.floor(Math.random() * 5)] +
+    noun[Math.floor(Math.random() * 5)] +
+    extensiones[Math.floor(Math.random() * 5)];
+
+  console.log(excuseP);
 };
